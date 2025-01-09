e3b65448a4d4376515627bbc80a85fbb16da9178
Tu Nombre <tu-email@example.com>
2025-01-08 21:54:37 -0400

    lab 5

--- src/Components/Core/OpenLink.jsx
+++ src/Components/Core/OpenLink.jsx
@@ -1,10 +1,12 @@
 const OpenLink = ({ redirectURL, logoImage }) => {
     return (
-        <div>
-            <a href={redirectURL} target="_blank">
+        <>
+            <span><a href={redirectURL} target="_blank">
                 <img src={logoImage} className="logo" alt="Vite logo"/>
             </a>
-        </div>
+            </span>
+            
+        </>
     );
 };
 
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,20 +1,25 @@
-import Body from '../Components/Home/Body.jsx';
-import Footer from '../Components/Home/Footer.jsx';
-import OpenLink from '../Components/Core/OpenLink.jsx';
+import Body from "../Components/Home/Body.jsx";
+import Footer from "../Components/Home/Footer.jsx";
+import OpenLink from "../Components/Core/OpenLink.jsx";
 
-import reactLogo from '../assets/react.svg'
-import viteLogo from '../assets/vite.svg'
-import githubLogo from '../assets/github.svg';
+import reactLogo from "../assets/react.svg";
+import viteLogo from "../assets/vite.svg";
+import githubLogo from "../assets/github.svg";
 const Home = () => {
   return (
+    <>
       <div>
-          <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo}/>
-          <OpenLink redirectURL="https://react.dev" logoImage={reactLogo}/>
-          <OpenLink redirectURL="https://github.com/herlan-lopez/desarrollo-frontend-react" logoImage={githubLogo}/>
-          
-          <Body />
-          <Footer />
+        <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo} />
+        <OpenLink redirectURL="https://react.dev" logoImage={reactLogo} />
+        <OpenLink
+          redirectURL="https://github.com/herlan-lopez/desarrollo-frontend-react"
+          logoImage={githubLogo}
+        />
       </div>
+
+      <Body />
+      <Footer />
+    </>
   );
 };
 
