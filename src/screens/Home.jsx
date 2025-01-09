d1098871aadc6fbb0f0024f63240730e635b45d7
Tu Nombre <tu-email@example.com>
2025-01-08 21:44:35 -0400

    crear props

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,17 @@
-import Logo from '../Components/Home/Logo.jsx';
 import Body from '../Components/Home/Body.jsx';
 import Footer from '../Components/Home/Footer.jsx';
-import Github from '../Components/Home/Github.jsx';
+import OpenLink from '../Components/Core/OpenLink.jsx';
 
+import reactLogo from '../assets/react.svg'
+import viteLogo from '../assets/vite.svg'
+import githubLogo from '../assets/github.svg';
 const Home = () => {
   return (
       <div>
-          <Logo />
-          <Github />
+          <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo}/>
+          <OpenLink redirectURL="https://react.dev" logoImage={reactLogo}/>
+          <OpenLink redirectURL="https://github.com/herlan-lopez/desarrollo-frontend-react" logoImage={githubLogo}/>
+          
           <Body />
           <Footer />
       </div>
