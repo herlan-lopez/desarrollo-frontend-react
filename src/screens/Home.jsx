bda68dfa5e2a9a62297ec337c1ff7b897aae81de
Tu Nombre <tu-email@example.com>
2025-01-09 21:34:27 -0400

    lab 6

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -5,9 +5,15 @@ import OpenLink from "../Components/Core/OpenLink.jsx";
 import reactLogo from "../assets/react.svg";
 import viteLogo from "../assets/vite.svg";
 import githubLogo from "../assets/github.svg";
+
+import { useSelector, useDispatch } from 'react-redux'
+
+
 const Home = () => {
+  const count = useSelector((state) => state.counter.value)
   return (
     <>
+    <div>Count value  <span>{count}</span></div>
       <div>
         <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo} />
         <OpenLink redirectURL="https://react.dev" logoImage={reactLogo} />
