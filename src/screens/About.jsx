4e85e53133e055e2fd067d61337006b8e9adfcb5
Tu Nombre <tu-email@example.com>
2025-01-14 22:03:21 -0400

    adds use state

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,8 +1,17 @@
-
+import { useState } from 'react'
+import {useSelector} from 'react-redux'
 const About = () => {
+  const [nameComponent,setNameComponent]=useState('About')
+  
+
+  const count =useSelector((state)=>state.product.initialValue)
   return (
     <>
     <p>Componente About</p>
+    Valor de InitialValue es <span>{ count }</span>
+
+    <p>Component name: {nameComponent}</p>
+    <button onClick={()=>{setNameComponent('Curso de React!!!')}}>Press</button>
     </>
   )
 }
